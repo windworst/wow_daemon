b8f5562380b4b7098705bee68d524295b1af8837
xbw <windworst@gmail.com>
2020-02-11 10:59:53 +0800

    dance

--- main.c
+++ main.c
@@ -7,20 +7,11 @@ int random() {
     return rand();
 }
 
-void handleHWND(HWND hwnd) {
-    if(NULL == hwnd) { return; }
-    if(GetForegroundWindow() == hwnd) {
-        printf("Active Window... Ignore\n");
-        return;
-    }
-
-    int keys[] = {VK_SPACE, VK_LEFT, VK_UP, VK_RIGHT, VK_DOWN};
-    int action = keys[random() % (sizeof(keys)/sizeof(*keys)) ];
-
-    printf("Send Action: %d\n", action);
-    SendMessage(hwnd, WM_KEYDOWN, action, 0);
-    Sleep(random() % 500);
-    SendMessage(hwnd, WM_KEYUP, action, 0 );
+void typeString(HWND hwnd, const char* str) {
+  for(int i = 0, c = 0; (c = str[i]) != '\0'; ++i) {
+    SendMessage(hwnd, WM_CHAR, c, 0);
+    Sleep(random() % 100);
+  }
 }
 
 BOOL CALLBACK EnumWindowsProc(
@@ -28,7 +19,7 @@ BOOL CALLBACK EnumWindowsProc(
   _In_ LPARAM lParam
 ) {
     char name[1000] = {0}, className[1000] = {0};
-    if(GetWindowText(hwnd, name, sizeof(name)) > 0, GetClassName(hwnd, className, sizeof(className)) > 0) {
+    if(hwnd != NULL && (GetWindowText(hwnd, name, sizeof(name)) > 0, GetClassName(hwnd, className, sizeof(className)) > 0) ) {
         if(strcmp(className, "GxWindowClass") == 0) //WOW
         {
             printf("Handle = %p ", hwnd);
@@ -36,15 +27,33 @@ BOOL CALLBACK EnumWindowsProc(
             printf("Class = '%s' ", className);
             printf("\n");
 
-            handleHWND(hwnd);
+            if(GetForegroundWindow() == hwnd) {
+                printf("Active Window... Ignore\n");
+                return TRUE;
+            }
+
+            printf("Send Keys\n");
+
+            SendMessage(hwnd, WM_KEYDOWN, VK_RETURN, 0);
+            Sleep(random() % 100);
+            SendMessage(hwnd, WM_KEYUP, VK_RETURN, 0 );
+            Sleep(random() % 100);
+            if (lParam == 0) {
+              typeString(hwnd, "/camp");
+            } else {
+              typeString(hwnd, "/dance");
+            }
+            SendMessage(hwnd, WM_KEYDOWN, VK_RETURN, 0);
+            Sleep(random() % 100);
+            SendMessage(hwnd, WM_KEYUP, VK_RETURN, 0 );
         }
     }
     return TRUE;
 }
 
 int main(int argc, char** argv) {
-    for(;;) {
-        if(!EnumWindows(EnumWindowsProc, 0)) {
+    for(int i = 1, n = 80;; ++i) {
+        if(!EnumWindows(EnumWindowsProc, i % n)) {
             printf("Enum Windows Failed\n");
         }
         Sleep(1000 * (random() % 10) );
